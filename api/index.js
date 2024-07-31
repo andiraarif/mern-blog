255e1db71f675a08761fa59b3ee3151bc696615a
Mohamad Arif Andira <arifandira0209@gmail.com>
2024-07-31 15:03:36 +0700

    Create a test API route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -8,7 +9,7 @@ mongoose.connect(process.env.MONGO)
     .then(() => {
         console.log("MongoDb is connected");
     }).catch((err) => {
-        console.log("Error");
+        console.log(err);
     })
 
 const app = express();
@@ -16,3 +17,5 @@ const app = express();
 app.listen(3000, () => {
     console.log("Server is running on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
